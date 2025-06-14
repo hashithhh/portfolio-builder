9504d6660924de3ea5e2ac5b2de1d52bdf3dd4df
Hashith <hashithalakunta907@gmail.com>
2025-06-14 13:48:39 +0530

    Fix routing: add root path redirect to dashboard

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
-import Preview from "./pages/Preview"; 
+import Preview from "./pages/Preview";
 import Publish from "./pages/Publish";
-import AuthPage from "./pages/AuthPage"
+import AuthPage from "./pages/AuthPage";
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<Navigate to="/dashboard" replace />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/preview" element={<Preview />} />
         <Route path="/publish" element={<Publish />} />
